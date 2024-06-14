81f187101048ae63f3dc3395ba9ae20b23ff000e
joao-montanari <joao.unipbr@gmail.com>
2024-06-14 20:02:32 -0300

    feat (pages) Adiciona os primeiros conteudos para a página Home.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 
-import 'style.sass';
+import './style.sass';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div id="home-page-container">
+            <div id="home-page-content">
+                <div id="home-page-content-infos">
+                    <h3>HI THERE!</h3>
+                    <h1>I´M JOÃO MONTANARI</h1>
+                    <label>SYSTEM DEVELOPMENT / FRONT END</label>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                        incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                        exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute 
+                        irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                    </p>
+                    <button>MORE ABOUT ME</button>
+                </div>
+            </div>
+            <div id="home-page-content-image"/>
         </div>
     );
 }
