14edae704f094e822c25ccc9e8d614fc47ac83af
joao-montanari <joao.unipbr@gmail.com>
2024-06-14 19:58:18 -0300

    feat (sidebar component) Criação do componente SideBar.

---
+++ src/components/SideMenu/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './style.sass';
+
+const SideMenu = () => {
+    return (
+        <div id="side-menu-container">
+            <div id="side-menu-image"/>
+        </div>
+    );
+}
+
+export default SideMenu;
