18e942ea3ab8dc20fb8ff751ce034638d8d65bf3
joao-montanari <joao.unipbr@gmail.com>
2025-04-14 16:43:02 -0300

    :tada: feat: novo commit inicial

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { RouterProvider } from 'react-router-dom';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-import routers from './router';
-
-import './styles/main.sass';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <RouterProvider router={routers} />
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@Components" : path.resolve(__dirname, 'src/components'),
-      "@Styles": path.resolve(__dirname, "src/styles"),
-      "@Pages": path.resolve(__dirname, "src/pages"),
-      "@Models": path.resolve(__dirname, "src/models"),
-      "@Utils": path.resolve(__dirname, "src/utils"),
-      "@Layouts": path.resolve(__dirname, "src/layouts"),
-    }
-  },
 })
