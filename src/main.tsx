401938865a04498f6178d937e6f074ad5d39250f
joao-montanari <joao.unipbr@gmail.com>
2025-04-15 08:54:01 -0300

    :lipstick: feat(sass) Adiciona o sass como linguagem de estilização

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom';
 
 import routers from './router';
-import './index.css'
+import './styles/main.sass';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
