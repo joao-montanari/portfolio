91bced1e8a065e62efcfe70657b9530929385db2
joao-montanari <joao.unipbr@gmail.com>
2025-04-15 08:04:33 -0300

    :wrench: chore(tsconfig): Configuração das rotas de import do projeto

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      "@Components" : path.resolve(__dirname, 'src/components'),
+      "@Styles": path.resolve(__dirname, "src/styles"),
+      "@Pages": path.resolve(__dirname, "src/pages"),
+      "@Models": path.resolve(__dirname, "src/models"),
+      "@Utils": path.resolve(__dirname, "src/utils"),
+      "@Layouts": path.resolve(__dirname, "src/layouts"),
+    }
+  },
 })
