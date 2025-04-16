95e2c315fa1c14f428c3185c70828cd33aae8da6
joao-montanari <joao.unipbr@gmail.com>
2025-04-16 09:19:51 -0300

    :sparkles: feat(components): Criação do componente Header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,47 @@
+import { useNavigate } from 'react-router-dom';
+import { Language, DataObject } from '@mui/icons-material';
+import { SvgIcon } from '@mui/material';
+
+import Button from '@Components/Button';
+import './style.sass';
+
+export interface HeaderProps {
+    design?: 'primary';
+}
+
+const links = [
+    { name: 'Início', url: '/home' },
+    { name: 'Projetos', url: '/projects' },
+    { name: 'Sobre mim', url: '/about-us' },
+]
+
+const Header = ({ design = 'primary' } : HeaderProps) => {
+    const navigate = useNavigate();
+
+    return (
+        <div 
+            className={[
+                'custom-header-component',
+                `custom-header-component-${design}`
+            ].join(' ')}
+        >
+            <div className='left-side-header'>
+                <SvgIcon component={DataObject} className='logo-header'/>
+            </div>
+            <div className='right-side-header'>
+                {
+                    links.map((link) => (
+                        <Button
+                            design='secondary'
+                            label={link.name}
+                            onClick={() => navigate(link.url)}
+                        />
+                    ))
+                }
+                <SvgIcon component={Language} className='select-lang-header'/>
+            </div>
+        </div>
+    )
+}
+
+export default Header;
