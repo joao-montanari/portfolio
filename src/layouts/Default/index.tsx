1467910871e00c07da1042b720a3b073375979b8
joao-montanari <joao.unipbr@gmail.com>
2025-04-16 09:20:53 -0300

    :recycle: refactor(layout): implementa o componente Header ao layout Default

--- src/layouts/Default/index.tsx
+++ src/layouts/Default/index.tsx
@@ -1,11 +1,17 @@
 import { Outlet } from "react-router-dom";
 
 import './styles.sass';
+import Header from "@Components/Header";
 
 const DefaultLayout = () => {
     return (
         <div className="default-layout-container">
-            <Outlet/>
+            <div className="default-layout-content">
+                <div className="header-container-layout">
+                    <Header/>
+                </div>
+                <Outlet/>
+            </div>
         </div>
     )
 }
