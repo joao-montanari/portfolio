bf1f5e3a9acd61e981fad0fa7111dd948f37845f
joao-montanari <joao.unipbr@gmail.com>
2025-04-16 09:22:20 -0300

    :recycle: refactor(pages): Remove o componente button da página inicial e adiciona classe de estilo a div principal da página

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,8 @@
-import Button from '@Components/Button';
 import './style.sass';
 
 const HomePage = () => {
     return (
-        <div>
-            <Button
-                label='Home'
-                onClick={() => {}}
-            />
+        <div className='home-page-container'>
         </div>
     )
 }
