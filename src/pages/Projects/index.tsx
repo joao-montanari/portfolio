c24c82f6a9d3a0465915312d14a2fc6a3a5c68f1
João Vitor Montanari <joao.unipbr@gmail.com>
2025-04-20 21:42:21 -0300

    :construction: refactor: lista os projetos para testar a aparência da página

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -1,12 +1,65 @@
-import Card from '@Components/Card';
+import Card, { CardProps } from '@Components/Card';
 import './style.sass';
+
+const projects: CardProps[] = [
+    {
+        title: 'Portfolio',
+        description: 'Meu portfolio pessoal que apresenta o meu perfil como desenvolvedor de sistemas',
+        image: 'https://github.com/joao-montanari.png',
+        repositoryUrl: 'https://github.com/joao-montanari/portfolio',
+        siteUrl: 'https://github.com/joao-montanari/portfolio',
+        technologies: [
+            'React',
+            'Vite',
+            'Sass',
+            'TypeScript'
+        ],
+    },
+    {
+        title: 'Portfolio',
+        description: 'Meu portfolio pessoal que apresenta o meu perfil como desenvolvedor de sistemas',
+        image: 'https://github.com/joao-montanari.png',
+        repositoryUrl: 'https://github.com/joao-montanari/portfolio',
+        siteUrl: 'https://github.com/joao-montanari/portfolio',
+        technologies: [
+            'React',
+            'Vite',
+            'Sass',
+            'TypeScript'
+        ],
+    },
+    {
+        title: 'Portfolio',
+        description: 'Meu portfolio pessoal que apresenta o meu perfil como desenvolvedor de sistemas',
+        image: 'https://github.com/joao-montanari.png',
+        repositoryUrl: 'https://github.com/joao-montanari/portfolio',
+        siteUrl: 'https://github.com/joao-montanari/portfolio',
+        technologies: [
+            'React',
+            'Vite',
+            'Sass',
+            'TypeScript'
+        ],
+    },
+]
 
 const ProjectsPage = () => {
     return (
         <div className='projects-page-container'>
             <h1>Projects</h1>
             <div className='projects-page-content'>
-                <Card/>
+                {
+                    projects.map((proj) => (
+                        <Card
+                            title={proj.title}
+                            description={proj.description}
+                            image={proj.image}
+                            repositoryUrl={proj.repositoryUrl}
+                            siteUrl={proj.siteUrl}
+                            technologies={proj.technologies}
+                        />
+                    ))
+                }
             </div>
         </div>
     )
