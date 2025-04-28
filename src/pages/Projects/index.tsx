6f5729b80c004168ddea59d163be9ae2bdf358b2
joao-montanari <joao.unipbr@gmail.com>
2025-04-28 16:44:48 -0300

    :recycle: refactor(pages): A página de projetos passa a renderizar os projetos com card secundario

--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -40,6 +40,35 @@ const projects: CardProps[] = [
             'Sass',
             'TypeScript'
         ],
+        design: 'secondary',
+    },
+    {
+        title: 'Portfolio',
+        description: 'Meu portfolio pessoal que apresenta o meu perfil como desenvolvedor de sistemas',
+        image: 'https://github.com/joao-montanari.png',
+        repositoryUrl: 'https://github.com/joao-montanari/portfolio',
+        siteUrl: 'https://github.com/joao-montanari/portfolio',
+        technologies: [
+            'React',
+            'Vite',
+            'Sass',
+            'TypeScript'
+        ],
+        design: 'secondary',
+    },
+    {
+        title: 'Portfolio',
+        description: 'Meu portfolio pessoal que apresenta o meu perfil como desenvolvedor de sistemas',
+        image: 'https://github.com/joao-montanari.png',
+        repositoryUrl: 'https://github.com/joao-montanari/portfolio',
+        siteUrl: 'https://github.com/joao-montanari/portfolio',
+        technologies: [
+            'React',
+            'Vite',
+            'Sass',
+            'TypeScript'
+        ],
+        design: 'secondary',
     },
 ]
 
@@ -50,15 +79,36 @@ const ProjectsPage = () => {
             <div className='projects-page-content'>
                 {
                     projects.map((proj) => (
-                        <Card
-                            title={proj.title}
-                            description={proj.description}
-                            image={proj.image}
-                            repositoryUrl={proj.repositoryUrl}
-                            siteUrl={proj.siteUrl}
-                            technologies={proj.technologies}
-                            style={{ width: '100%', height: '90%' }}
-                        />
+                        proj.design === 'primary' || proj.design === undefined && (
+                            <Card
+                                title={proj.title}
+                                description={proj.description}
+                                image={proj.image}
+                                repositoryUrl={proj.repositoryUrl}
+                                siteUrl={proj.siteUrl}
+                                technologies={proj.technologies}
+                                design={proj.design}
+                                style={{ width: '100%', height: '100%' }}
+                            />
+                        )
+                    ))
+                }
+            </div>
+            <div className='projects-page-content-grid-3-for-1'>
+                {
+                    projects.map((proj) => (
+                        proj.design === 'secondary' && (
+                            <Card
+                                title={proj.title}
+                                description={proj.description}
+                                image={proj.image}
+                                repositoryUrl={proj.repositoryUrl}
+                                siteUrl={proj.siteUrl}
+                                technologies={proj.technologies}
+                                design={proj.design}
+                                style={{ width: '100%', height: '100%' }}
+                            />
+                        )
                     ))
                 }
             </div>
